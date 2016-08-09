313983f73f8a70c572c0d15d108289e57dfc3264
Brian Perera <briano_p@yahoo.com>
2016-08-09 21:50:06 +0530

    UI modifications

---
+++ Source/EasyMaintain.Application/EasyMaintain.WebUI/Utility/SessionUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyMaintain.WebUI.Utility
+{
+    public static class SessionUtility
+    {
+        public static string SparePartsModel = "SparePartsModel";
+    }
+}
