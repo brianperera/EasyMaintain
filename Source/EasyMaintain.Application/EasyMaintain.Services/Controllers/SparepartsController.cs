8402dfb7e3795ca667304ceb3a579cd71e42883d
johnatm <john.atm@gmail.com>
2016-08-29 17:51:03 +0530

    Spare Parts web service controller and other updates

---
+++ Source/EasyMaintain.Application/EasyMaintain.Services/Controllers/SparepartsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EasyMaintain.Business;
+using EasyMaintain.Services.Models;
+
+namespace EasyMaintain.Services.Controllers
+{
+    public class SparepartsController : ApiController
+    {
+        private EasyMaintainServicesContext db = new EasyMaintainServicesContext();
+
+        // GET api/Spareparts
+        public IQueryable<SparePart> GetSpareParts()
+        {
+            var spareparts = from b in db.SpareParts
+                             select new SparePart()
+                             {
+
+                                 SparePartID = b.SparePartID,
+                                 Category = b.Category,
+                                 SparePartName = b.SparePartName,
+                                 Description = b.Description
+                             };
+
+
+            return db.SpareParts;
+        }
+
+        // GET api/Spareparts/5
+        [ResponseType(typeof(SparePart))]
+        public async Task<IHttpActionResult> GetSparePart(int id)
+        {
+            SparePart sparepart = await db.SpareParts.FindAsync(id);
+            if (sparepart == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sparepart);
+        }
+
+        // PUT api/Spareparts/5
+        public async Task<IHttpActionResult> PutSparePart(int id, SparePart sparepart)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sparepart.SparePartID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(sparepart).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SparePartExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Spareparts
+        [ResponseType(typeof(SparePart))]
+        public async Task<IHttpActionResult> PostSparePart(SparePart sparepart)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.SpareParts.Add(sparepart);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = sparepart.SparePartID }, sparepart);
+        }
+
+        // DELETE api/Spareparts/5
+        [ResponseType(typeof(SparePart))]
+        public async Task<IHttpActionResult> DeleteSparePart(int id)
+        {
+            SparePart sparepart = await db.SpareParts.FindAsync(id);
+            if (sparepart == null)
+            {
+                return NotFound();
+            }
+
+            db.SpareParts.Remove(sparepart);
+            await db.SaveChangesAsync();
+
+            return Ok(sparepart);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SparePartExists(int id)
+        {
+            return db.SpareParts.Count(e => e.SparePartID == id) > 0;
+        }
+    }
+}
---
+++ Source/EasyMaintain.Application/EasyMaintain.Services/Models/Spareparts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EasyMaintain.Business;
+
+
+
+
+namespace EasyMaintain.Services.Models
+{
+    public class Spareparts
+    {
+        public string Description { get; set; }
+        public int AircrafModelId { get; set; }
+        public int ManufacturerId { get; set; }
+        public int SupplierId { get; set; }
+
+        public IEnumerable<AircraftModel> AircrafModels { get; set; }
+        public IEnumerable<Manufacturer> Manufacturers { get; set; }
+        public IEnumerable<Supplier> Suppliers { get; set; }
+        public List<SparePart> SpareParts { get; set; }
+
+
+
+
+    }
+}
