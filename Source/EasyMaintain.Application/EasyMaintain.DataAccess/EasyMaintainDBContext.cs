9ee9d5f23cfd3c0631501eae5f4efd250e13c537
johnatm <john.atm@gmail.com>
2016-09-07 18:11:06 +0530

    Check-in the latest service fabric projects

---
+++ Source/EasyMaintain.Application/EasyMaintain.DataAccess/EasyMaintainDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMaintain.DataAccess
+{
+    class EasyMaintainDBContext:DbContext
+    {
+
+        public EasyMaintainDBContext():base()
+        {
+
+        }
+
+
+       public DbSet<AircraftModel> AircraftModels { get;set; }
+       public DbSet<Category> Categories { get; set; }
+       public DbSet<EngineType> EngineTypes { get; set; }
+       public DbSet<Manufacturer> Manufactureres { get; set; }
+      public  DbSet<SparePart> SpareParts { get; set; }
+       public DbSet<Supplier> Suppliers { get; set; }
+
+    }
+}
