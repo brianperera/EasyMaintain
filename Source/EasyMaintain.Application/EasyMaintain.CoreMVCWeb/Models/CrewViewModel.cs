6c7246ed196d69c23f5f3253790ccf9acee12afc
Madushan Fernando <madushan11@live.com>
2016-09-21 10:40:17 +0530

    Crew Model

---
+++ Source/EasyMaintain.Application/EasyMaintain.CoreMVCWeb/Models/CrewViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyMaintain.CoreWebMVC.Models
+{
+    public class Crew
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+    }
+
+    public class CrewViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+
+        public int currentIndex { get; set; }
+        public List<Crew> CrewList { get; set; }
+        public CrewViewModel()
+        {
+            CrewList = new List<Crew>();
+            CrewList.Add(new Crew() {ID = 1, Name = "Jann", Designation = "Head Mechanic" });
+        }
+    }
+}
