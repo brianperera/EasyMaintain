60873c8e513b3194048008c8e4fa44786bc4138e
johnatm <john.atm@gmail.com>
2016-09-27 10:38:59 +0530

    New business logic project on .net core

--- Source/EasyMaintain.Application/EasyMaintain.Business/Entities/Maintenance.cs
+++ Source/EasyMaintain.Application/EasyMaintain.Business/Entities/Maintenance.cs
@@ -6,24 +6,26 @@ using System.Threading.Tasks;
 using EasyMaintain.DataAccess;
 using EasyMaintain.Business.Entities;
 
+
+
 namespace EasyMaintain.Business
 {
     public class Maintenance : IBusiness
     {
 
         Maintenance mMaintenance;
-     
+
         public int WorkID { get; set; }
         public string FlightModel { get; set; }
         public string FlightNumber { get; set; }
-        public string  Description { get; set; }
+        public string Description { get; set; }
         public string StartDate { get; set; }
         public string CompletionDate { get; set; }
         public string Location { get; set; }
         public List<MaintenanceChecks> CheckItems { get; set; }
         public List<Crew> CrewMembers { get; set; }
 
-        
+
 
         /// <summary>
         /// Get data
