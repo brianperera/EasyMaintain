72647ae2065d23c45e13a31ab0b7c438572b8b67
Madushan Fernando <madushan11@live.com>
2016-09-28 15:05:45 +0530

    Models Moved to DTO

---
+++ Source/EasyMaintain.Application/EasyMaintain.DTO/Models/Component.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyMaintain.DTO
+{
+    public class Component
+    {
+        public int ComponentID { get; set; }
+        public string Category { get; set; }
+        public string ComponentName { get; set; }
+        public string Manufacturer { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public string AdditionalData { get; set; }
+    }
+}
---
+++ Source/EasyMaintain.Application/EasyMaintain.DTO/Models/Manufacturer.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyMaintain.DTO
+{
+    public class Manufacturer
+    {
+        public string ManufacturerID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AdditionalData { get; set; }
+    }
+}
