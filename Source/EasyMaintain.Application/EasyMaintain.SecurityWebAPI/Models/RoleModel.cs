e1f6bb2a7af857d2477f03e0bf6455ee12c7fc52
johnatm <john.atm@gmail.com>
2016-10-24 11:58:57 +0530

    New models added

---
+++ Source/EasyMaintain.Application/EasyMaintain.SecurityWebAPI/Models/RoleModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Routing;
+
+namespace EasyMaintain.SecurityWebAPI.Models
+{
+   public class RoleModel
+    {
+        private UrlHelper _UrlHelper;
+        private ApplicationUserManager _AppUserManager;
+
+
+
+        public RoleModel(HttpRequestMessage request, ApplicationUserManager appUserManager)
+        {
+            _UrlHelper = new UrlHelper(request);
+            _AppUserManager = appUserManager;
+        }
+        public RoleReturnModel Create(IdentityRole appRole)
+        {
+
+            return new RoleReturnModel
+            {
+                Url = _UrlHelper.Link("GetRoleById", new { id = appRole.Id }),
+                Id = appRole.Id,
+                Name = appRole.Name
+            };
+        }
+    }
+
+    public class RoleReturnModel
+    {
+        public string Url { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
+
